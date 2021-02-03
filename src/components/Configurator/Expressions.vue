3f0bd940b759c15481e4c7e06d5ce14c87b645ce
Jože Fortun <joze.fortun@gmail.com>
2021-02-04 00:56:10 +0100

    Update Expressions configuratior UI

--- src/components/Configurator/Expressions.vue
+++ src/components/Configurator/Expressions.vue
@@ -16,34 +16,65 @@
         </div>
       </template>
 
-      <b-input-group
-        v-for="(argument, index) in item.config.arguments"
-        :key="index"
-        class="mb-2"
+      <b-table
+        v-if="item.config.arguments.length"
+        id="arguments"
+        outlined
+        fixed
+        head-variant="light"
+        :items="item.config.arguments"
+        :fields="argumentFields"
+        @row-clicked="item=>$set(item, '_showDetails', !item._showDetails)"
       >
-        <b-form-input
-          v-model="argument.target"
-          placeholder="Target"
-        />
+        <template #cell(type)="{ item: a }">
+          <var>{{ a.type }}</var>
+        </template>
 
-        <b-form-select
-          v-model="argument.type"
-          :options="fieldTypes"
-        />
+        <template #cell(value)="{ item: a }">
+          <samp>{{ a.expr }}</samp>
+        </template>
 
-        <b-form-input
-          v-model="argument.expr"
-          placeholder="Expression"
-        />
+        <template #row-details="{ index, item: a }">
+          <b-form-group
+            label="Target"
+          >
+            <b-form-input
+              v-model="a.target"
+              placeholder="Target"
+            />
+          </b-form-group>
 
-        <b-button
-          class="ml-1"
-          variant="danger"
-          @click="removeArgument(index)"
-        >
-          X
-        </b-button>
-      </b-input-group>
+          <b-form-group
+            label="Type"
+          >
+            <b-form-select
+              v-model="a.type"
+              :options="fieldTypes"
+            />
+          </b-form-group>
+
+          <b-form-group
+            label="Expression"
+          >
+            <b-form-input
+              v-model="a.expr"
+              placeholder="Expression"
+            />
+          </b-form-group>
+
+          <div
+            class="d-inline-block bg-white w-auto"
+          >
+            <c-input-confirm
+              size="md"
+              :borderless="false"
+              @confirmed="removeArgument(index)"
+            >
+              Remove
+            </c-input-confirm>
+          </div>
+        </template>
+      </b-table>
     </b-form-group>
   </div>
 </template>
@@ -56,15 +87,28 @@ export default {
 
   data () {
     return {
-      types: []
+      fieldTypes: []
     }
   },
 
   computed: {
-    fieldTypes () {
+    argumentFields () {
       return [
-        { value: '', text: 'Select type', disabled: true },
-        ...this.types
+        {
+          key: 'target',
+          label: 'Name',
+          tdClass: 'border-top text-truncate pointer'
+        },
+        {
+          key: 'type',
+          class: 'text-center',
+          tdClass: 'border-top text-truncate pointer'
+        },
+        {
+          key: 'value',
+          class: 'text-right',
+          tdClass: 'border-top text-truncate pointer'
+        },
       ]
     },
   },
@@ -81,7 +125,8 @@ export default {
       this.item.config.arguments.push({
         target: '',
         expr: '',
-        type: ''
+        type: 'Any',
+        _showDetails: true
       })
     },
 
@@ -91,7 +136,7 @@ export default {
 
     async getTypes () {
       return this.$AutomationAPI.typeList()
-        .then(({ set }) => this.types = set)
+        .then(({ set }) => this.fieldTypes = set)
         .catch(this.defaultErrorHandler('Failed to fetch types'))
     }
   }
