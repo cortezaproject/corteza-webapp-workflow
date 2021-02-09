85b463be90dbe3072797c3851e5de7fb33f0e0f5
Jože Fortun <joze.fortun@gmail.com>
2021-02-09 10:58:44 +0100

    Change canvas background image

--- src/components/WorkflowEditor.vue
+++ src/components/WorkflowEditor.vue
@@ -252,7 +252,7 @@ export default {
       this.styling()
       this.connectionHandler()
 
-      this.graph.container.style.background = `url('${require('../assets/grid.gif')}')`
+      this.graph.container.style.background = 'url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAiIGhlaWdodD0iNDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGRlZnM+PHBhdHRlcm4gaWQ9ImdyaWQiIHdpZHRoPSI0MCIgaGVpZ2h0PSI0MCIgcGF0dGVyblVuaXRzPSJ1c2VyU3BhY2VPblVzZSI+PHBhdGggZD0iTSAwIDEwIEwgNDAgMTAgTSAxMCAwIEwgMTAgNDAgTSAwIDIwIEwgNDAgMjAgTSAyMCAwIEwgMjAgNDAgTSAwIDMwIEwgNDAgMzAgTSAzMCAwIEwgMzAgNDAiIGZpbGw9Im5vbmUiIHN0cm9rZT0iI2QwZDBkMCIgb3BhY2l0eT0iMC4yIiBzdHJva2Utd2lkdGg9IjEiLz48cGF0aCBkPSJNIDQwIDAgTCAwIDAgMCA0MCIgZmlsbD0ibm9uZSIgc3Ryb2tlPSIjZDBkMGQwIiBzdHJva2Utd2lkdGg9IjEiLz48L3BhdHRlcm4+PC9kZWZzPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9InVybCgjZ3JpZCkiLz48L3N2Zz4=")'
 
       this.render(this.workflow)
     } catch (e) {
@@ -295,7 +295,7 @@ export default {
 
       this.graph.setConnectable(true)
       this.graph.setAllowDanglingEdges(false)
-      this.graph.view.setTranslate(20, 20)
+      this.graph.view.setTranslate(10, 10)
 
       // Enables guides
       mxGraphHandler.prototype.guidesEnabled = true
