90b15b7a65a44f98025816e5e18cf2f43fc0baed
Jože Fortun <joze.fortun@gmail.com>
2022-03-18 20:00:34 +0100

    Add initial tests

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,11 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset',
   ],
+  env: {
+    test: {
+      plugins: [
+        ['istanbul', { useInlineSourceMaps: false }],
+      ],
+    },
+  },
 }
